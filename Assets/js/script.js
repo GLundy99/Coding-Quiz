14647bf4a9f31e13114b9e98947289582120b389
Griffin Lundquist <griffin@arcadiaitpartners.com>
2022-04-10 16:59:39 -0500

    code restructer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,87 +1,42 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-    function showQuestions(questions, quizContainer) {
-    }
-    function showResults(questions, quizContainer, resultsContainer) {
-    }
-    showQuestions(questions, quizContainer);
-    submitButton.onclick = function() {
-        showResults(questions, quizContainer, resultsContainer);
-    }
-}
-
-var myQuestions = [
+let questions = [
     {
+        qNumber: "1. ",
         questions: "Var stands for variable",
-        answer: {
-            a: "True",
-            b: "False",
-        },
-        correctAnswer: "a"
+        answer: "True",
+        options: [
+             "True",
+             "False",
+        ]
     },
-    {
+        {
+        qNumber: "2. ",   
         question: "What folder does the css file go in?",
-        answer: {
-            a: "html",
-            b: "Readme",
-            c: "assets",
-        },
-        correctAnswer: "c"
+        answer: "assets",
+        options: [
+             "html",
+             "Readme",
+             "assets",
+        ]
     },
-    {
+        {
+        qNumber: "3. ",
         question: "Are the Minnesota Wild going to win the Stanley Cup this year?",
-        answer: {
-            a: "Yes",
-            b: "No",
-        },
-        correctAnswer: "a"
+        answer: "Yes",
+        options: [
+             "Yes",
+             "No",
+        ]
+    },
+        {
+        qNumber: "4. ",
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+             "Cascading Stlye Sheet",
+             "Coding Style Systems",
+             "Cooper System Setup",
+             "Coding Structer Standered",
+        ]
     },
 ];
 
-function showQuestions(questions, quizContainer) {
-    var output = [];
-    var answer;
-
-    for(var i=0; i<questions.length; i++) {
-        answer = [];
-        for(letter in question[i].answer) {
-            answer.push(
-                "<label>"
-                   + "<input type='radio' name='question'"+i+" value="+letter+">"
-                   + letter + ":"
-                   +questions[i].answer[letter]
-                +  "</label>"
-            );
-        }
-        outpush.push(
-            '<div class="questions">' + questions[i].question + '</div>'
-            + '<div class="answers">' + answer.join("") + '</div>'
-        );
-    }
-    quizContainer.innerHTML = output.join("");
-}
-showQuestions(question, quizContainer);
-
-function showResults(questions, quizContainer, resultsContainer) {
-    var answerContainers = quizContainer.querySelectorAll(".answers");
-    var userAnswer = "";
-    var numCorrect = 0;
-    for(var i=0; i<questions.length; i++){
-        userAnswer = (answerContainers[i].querySelector("input[name=question"+i+"]:checked")||{}).value;
-        if(userAnswer === questions[i].correctAnswer) {
-            numCorrect++;
-            answerContainers[i].style.color = "purple";
-        }
-        else {
-            answerContainers[i].style.color = "red";
-        }
-    }
-    resultsContainer.innerHTML = numCorrect + "out of" + questions.length;
-}
-submitButton.onclick = function() {
-    showResults(question, quizContainer, resultsContainer);
-}
-
-var quizContainer = document.getElementById("quiz");
-var resultsContainer = document.getElementById("results");
-var submitButton = document.getElementById("submit");
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
